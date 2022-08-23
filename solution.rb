c7e3a3ac1fd2c4079d893edbcd7ca0520dd230db
GUIPARO <guisela2728@gmail.com>
2022-08-23 16:12:54 -0500

    fix bug

--- solution.rb
+++ solution.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  if  !(params[:nombre]) || params[:nombre] == ""
+  if  !(params[:nombre]) || params[:nombre] == "".strip
     "<h1>Hola desconocido !</h1>"
   else
     "<h1>Hola #{params[:nombre]} !</h1>"
