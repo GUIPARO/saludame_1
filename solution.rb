2bb882a0511e80fc73df5f9dc84d9827d74b1775
GUIPARO <guisela2728@gmail.com>
2022-08-23 16:11:51 -0500

    fix bug

--- solution.rb
+++ solution.rb
@@ -1,14 +1,9 @@
 require 'sinatra'
 
 get '/' do
-  if params[:nombre] && params[:nombre] != ""
-		<<-HTML
-			<h1>Hola #{params[:nombre].capitalize}!</h1>
-		HTML
-	else
-		<<-HTML
-			<h1>Hola desconocido</h1>
-		HTML
-	end
+  if  !(params[:nombre]) || params[:nombre] == ""
+    "<h1>Hola desconocido !</h1>"
+  else
+    "<h1>Hola #{params[:nombre]} !</h1>"
+  end
 end
-
