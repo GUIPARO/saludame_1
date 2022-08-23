e4af6062cd6817d4030eca854756796906ad7700
GUIPARO <guisela2728@gmail.com>
2022-08-23 14:26:30 -0500

    change file name

--- saludame.rb
+++
@@ -1,9 +0,0 @@
-# Requerir la librería ---> Todo lo que necesita para utilizar los metodos que componene el DSL de Sinatra
-require 'sinatra'
-
-# Definir ruta --> get post patch put & delete
-
-get '/' do
-  @nombre = params[:nombre]
-  erb :index
-end 
