8c95b12f7b6184763980110ca27a95d5f63818c1
GUIPARO <guisela2728@gmail.com>
2022-08-23 16:04:48 -0500

    fix bug

--- solution.rb
+++ solution.rb
@@ -1,16 +1,14 @@
-# Requerir la librería ---> Todo lo que necesita para utilizar los metodos que componene el DSL de Sinatra
 require 'sinatra'
-require "sinatra/reloader" if development?
-
-# Definir ruta --> get post patch put & delete
 
 get '/' do
-  if params[:nombre] && params[:nombre] != ''
-			"<h1>Hola #{params[:nombre].capitalize}!</h1>"
+  if params[:nombre] && params[:nombre] != nil
+		<<-HTML
+			<h1>Hola #{params[:nombre].capitalize}!</h1>
+		HTML
 	else
-			'<h1>Hola desconocido</h1>'
+		<<-HTML
+			<h1>Hola desconocido</h1>
+		HTML
 	end
 end
-
-
 
