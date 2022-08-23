8204d3bcc4b1fd14695a99eaf558c34dddc1e30f
GUIPARO <guisela2728@gmail.com>
2022-08-23 15:04:45 -0500

    fix solution.rb

--- solution.rb
+++ solution.rb
@@ -1,12 +1,15 @@
 # Requerir la librería ---> Todo lo que necesita para utilizar los metodos que componene el DSL de Sinatra
 require 'sinatra'
+require "sinatra/reloader" if development?
 
 # Definir ruta --> get post patch put & delete
 
 get '/' do
   if params[:nombre]
     @nombre = params[:nombre]
-    erb :index
+    <<-HTML
+       <h1>Hola #{params[:nombre]}!</h1>
+    HTML
   else
     "Hola desconocido"
   end
