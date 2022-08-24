e6e7bf181c7d3f9aa0477c05cdeda3a1c083b1a9
GUIPARO <guisela2728@gmail.com>
2022-08-23 21:49:50 -0500

    reto saludame_2

--- solution.rb
+++ solution.rb
@@ -1,9 +1,9 @@
 require 'sinatra'
 
-get '/' do
+get '/makers/:nombre' do
   if params[:nombre] == nil ||  params[:nombre] == ""
     "<h1>Hola desconocido!</h1>"
   else
-    "<h1>Hola #{params[:nombre]}!</h1>"
+    "<h1>Hola #{params[:nombre].capitalize}!</h1>"
   end
 end
